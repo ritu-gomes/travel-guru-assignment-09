52755a7694483c93fa8eb908621b5e6f945ce8f8
ritu-gomes <irengomes018@gmail.com>
2020-09-19 18:53:19 +0600

    updated

--- src/componants/NavBar/NavBar.js
+++ src/componants/NavBar/NavBar.js
@@ -7,16 +7,14 @@ import "./navbar.css";
 const NavBar = () => {
     const [loggedUser,setLoggedUser] = useContext(userContext);
     return (
-        <Container>
+        <Container style={{backgroundColor: "rgba(0,0,0,0.0)"}}>
             <Navbar style={{backgroundColor: "rgba(0,0,0,0.0)"}}>
-                <Navbar.Brand href="/home"><img className="logo" src={logo} alt="logo"/></Navbar.Brand>
+                <Navbar.Brand href="/Home"><img className="logo" src={logo} alt="logo"/></Navbar.Brand>
                 <Form inline>
                     <FormControl type="text" placeholder="Search your destination" className="mr-sm-2" />
                     <Button variant="light">Search</Button>
                 </Form>
                 <Nav className="ml-auto">
-                    <Nav.Link href="/booking">Destination</Nav.Link>
-                    <Nav.Link href="#pricing">booking</Nav.Link>
                     <Nav.Link href="/Login">
                         <Button style={{backgroundColor: "#F9A51A",border: "none"}}>Log in</Button>
                     </Nav.Link>
